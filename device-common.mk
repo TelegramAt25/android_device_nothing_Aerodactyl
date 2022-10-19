6eb83150e91f74aa38f1133a906e786206894f77
Lunarixus Name <Nathan@lunarixus.dev>
2022-10-19 10:32:42 +0545

    Aerodactyl: remove unwanted packages
    Co-authored-by: bishwofic <bishwobhusal75@gmail.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -279,6 +279,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    remove_packages
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
