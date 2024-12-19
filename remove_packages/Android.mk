b3e7ce4ff547103a667f6522dd6446d3d1ae85e5
fukiame <fukiame@proton.me>
2024-12-19 23:07:59 +0700

    Aerodactyl: remove_packages: add an option to forcefully nuke AudioFX
    Signed-off-by: fukiame <fukiame@proton.me>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -11,6 +11,12 @@ LOCAL_OVERRIDES_PACKAGES := \
     PrebuiltGmail \
     QuickSearchBox \
     YouTube
+ifeq ($(NUKE_AUDIOFX),true)
+TARGET_EXCLUDES_AUDIOFX := true
+LOCAL_OVERRIDES_PACKAGES += \
+    AudioFX \
+    MusicFX
+endif
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
