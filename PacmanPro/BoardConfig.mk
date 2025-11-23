470d40bc78aaa88a5ecef8e06f023710d2eedec2
claxten10 <claxten10@gmail.com>
2025-11-23 21:42:03 +0000

    Aerodactyl: Set product marketname
    * Used by camera.
    
    [fukiame: going to be used for setting default device name]
    
    Change-Id: I30e7333b223f6efca8034d8e2b5caaf939aaa420
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- PacmanPro/BoardConfig.mk
+++ PacmanPro/BoardConfig.mk
@@ -11,6 +11,7 @@ TARGET_BOOTLOADER_BOARD_NAME := PacmanPro
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/PacmanPro/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/PacmanPro/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/PacmanPro/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/PacmanPro/vendor.prop
 
