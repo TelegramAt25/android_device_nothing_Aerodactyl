791dc2297596a1fcb08cb1137371475b54c49b72
fukiame <fukiame@proton.me>
2025-12-08 17:49:53 +0700

    Aerodactyl: initial Miku UI bringup
    Signed-off-by: fukiame <fukiame@proton.me>

--- miku_PacmanPro.mk
+++ miku_PacmanPro.mk
@@ -6,8 +6,8 @@
 # Inherit from the custom device configuration.
 $(call inherit-product, device/nothing/Aerodactyl/device-PacmanPro.mk)
 
-# Inherit from the LineageOS configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Miku UI stuff.
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := PacmanPro
