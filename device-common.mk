26c620239c0f105baf8ff3788444a894e15bad16
sidharthify <wednisegit@gmail.com>
2025-12-08 17:47:37 +0700

    Aerodactyl: drop Lineage overlays
    [fukiame: original commit: https://github.com/Nothing-2A/android_device_nothing_Aerodactyl/commit/abcaafa584d4307e2343288ce6d2d5580d20c3e0]
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -224,13 +224,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
 
-PRODUCT_PACKAGES += \
-    LineageApertureResTarget \
-    LineageSDKResTarget \
-    LineageSettingsProviderResTarget \
-    LineageSettingsResTarget \
-    LineageSystemUIResTarget
-
 # Page size
 PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
 
