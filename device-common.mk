31cfc2ae1eb1b8d4cd58b3640db4ccbf4540080a
fukiame <fukiame@proton.me>
2025-12-09 12:07:28 +0700

    fixup! Aerodactyl: overlay: restore Aperture
    Aperture overlay was dropped during YAAP bringup at [1], making all the configs made during [2] unavailable.
    Restore it.
    
    [1]: https://github.com/Nothing-2A/android_device_nothing_Aerodactyl/commit/abcaafa584d4307e2343288ce6d2d5580d20c3e0
    [2]: b6bf5ce7c240aec6313ecb7ad74afe831df5facd
    
    Fix: https://github.com/Nothing-2A/releases/issues/7
    Signed-off-by: fukiame <fukiame@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -237,6 +237,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
 
+PRODUCT_PACKAGES += \
+    MikuApertureResTarget
+
 # Page size
 PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
 
