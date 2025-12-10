54352b0e9bb271a9aef0d617e030815e06dcd517
fukiame <fukiame@proton.me>
2025-12-10 08:35:45 +0700

    Aerodactyl: manually build charger_res_images_vendor
    attempt to fix empty offline charging
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -168,6 +168,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+PRODUCT_PACKAGES += \
+    charger_res_images_vendor
+
 # Init
 PRODUCT_PACKAGES += \
     init.ethtooloffloading.rc \
