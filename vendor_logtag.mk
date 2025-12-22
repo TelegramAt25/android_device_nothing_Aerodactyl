cb450e1ed854ed3fdd594eb0173d296da048f493
fukiame <fukiame@proton.me>
2025-12-22 22:55:20 +0700

    Aerodactyl: vendor_logtag: silence usb audio spam
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerBase: getQueuedFramesInfo(), mPcm NULL!!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    W AudioALSAPlaybackHandlerUsb: write(), mPcm is NULL!
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -239,3 +239,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.PhoneConfigurationSettings=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.AudioALSAPlaybackHandlerBase=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.AudioALSAPlaybackHandlerUsb=$(VENDOR_LOG_LEVEL) \
