8564cbd8e4ca4b85d1242c48214054189a1e9fb7
fukiame <fukiame@proton.me>
2026-04-03 11:15:05 +0700

    fixup! Aerodactyl: inherit telephony-common-stub
    Signed-off-by: fukiame <fukiame@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -317,7 +317,7 @@ include $(LOCAL_PATH)/vendor_logtag.mk
 # Radio
 ENABLE_VENDOR_RIL_SERVICE := true
 
-$(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+$(call inherit-product, hardware/miku/compat/frameworks/compat.mk)
 $(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_COPY_FILES += \
