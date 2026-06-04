56f9fc8ddf11463f92ce9b6586a26213e64d6b3a
Mashopy <eliasgheeraert@gmail.com>
2026-06-04 20:12:22 +0200

    Aerodactyl: Properly enable LazyRCU

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -51,7 +51,7 @@ BOARD_KERNEL_CMDLINE += cgroup_disable=memory
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
 BOARD_KERNEL_CMDLINE += initcall_debug=0 loglevel=0 log_buf_len=1024K
 BOARD_KERNEL_CMDLINE += kasan=off
-BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy=1
+BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy
 BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 
 BOARD_INIT_BOOT_HEADER_VERSION := 4
